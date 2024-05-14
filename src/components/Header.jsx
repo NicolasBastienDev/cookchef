6ebe3ad74adcff008c55537ac76813d6580a5005
NicolasBastienDev <nicolas.bastien00@gmail.com>
2024-05-14 16:51:43 +0200

    add some RWD / new feature : search bar / better data

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,38 @@
-{/* <i class="fa-solid fa-bars"></i> */}
-import styles from "./Header.module.scss"
-import cookchef from "../assets/images/cookchef.png"
+{
+  /* <i class="fa-solid fa-bars"></i> */
+}
+import styles from "./Header.module.scss";
+import cookchef from "../assets/images/cookchef.png";
+import { useState } from "react";
+import HeaderMenu from "./HeaderMenu";
 
 function Header() {
-    return (
-        <header className={ `${ styles.header } d-flex flex-row align-items-center` }>
-            <i className="fa-solid fa-bars mr-15"></i>
-            <div className="flex-fill">
-                <img src={ cookchef } alt="logo cookchef" />
-            </div>
-            <ul>
-                <button className="mr-5 btn btn-reverse-primary">
-                <i className="fa-solid fa-basket-shopping mr-5"></i>
-                    <span>panier</span>
-                </button>
-                <button className="btn btn-primary">
-                    connexion
-                </button>
-            </ul>
-        </header>
-    );
+  const [showMenu, setShowMenu] = useState(false);
+
+  return (
+    <header className={`${styles.header} d-flex flex-row align-items-center`}>
+      <div className="flex-fill">
+        <img src={cookchef} alt="logo cookchef" />
+      </div>
+      <ul className={styles.headerList}>
+        <button className="mr-5 btn btn-reverse-primary">
+          <i className="fa-solid fa-heart mr-5"></i>
+          <span>Wish List</span>
+        </button>
+        <button className="btn btn-primary">connexion</button>
+      </ul>
+      <i
+        className={`fa-solid fa-bars mr-15 ${styles.headerXs}`}
+        onClick={() => setShowMenu(true)}
+      ></i>
+      {showMenu && (
+        <>
+          <div className="calc" onClick={() => setShowMenu(false)}></div>
+          <HeaderMenu />
+        </>
+      )}
+    </header>
+  );
 }
 
 export default Header;
--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,19 +1,26 @@
-import styles from './Recipe.module.scss'
-import recipe from '../assets/images/recette.jpg'
+import styles from "./Recipe.module.scss";
+import { useState } from "react";
 
-function Recipe() {
-    return(
-        <div className={ styles.recipe }>
-            <div className={ styles.imageContainer }>
-                <img src={ recipe } alt="recipe"/>
-            </div>
-            <div className={ `${styles.recipeTitle} d-flex flex-row justify-content-center align-items-center` }>
-                <h3>
-                    Carottes pomme de terre
-                </h3>
-            </div>
-        </div>
-    )
+function Recipe({ title, image }) {
+  const [liked, setLiked] = useState(false);
+
+  function handleClick() {
+    setLiked(!liked);
+  }
+
+  return (
+    <div onClick={handleClick} className={styles.recipe}>
+      <div className={styles.imageContainer}>
+        <img src={image} alt="recipe" />
+      </div>
+      <div
+        className={`${styles.recipeTitle} d-flex flex-column justify-content-center align-items-center`}
+      >
+        <h3 className="mb-10">{title}</h3>
+        <i className={`fa-solid fa-heart ${liked ? "text-primary" : ""}`}></i>
+      </div>
+    </div>
+  );
 }
 
 export default Recipe;
